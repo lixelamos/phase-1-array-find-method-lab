6234ac8712bede4c88de36cf1243930cba8d199f
lixelamos <amosbett99@gmail.com>
2023-03-23 23:50:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(records) {
+  const winRecord = records.find(record => record.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
+
+console.log(superbowlWin(records)); 
